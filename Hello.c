0b839c29a3b9ba1dd2447d1ff5fdefed6d28a6d5
69010389 <69010389@kmitl.ac.th>
2026-09-23 18:14:30 +0700

    change farewell 4

--- Hello.c
+++ Hello.c
@@ -3,6 +3,8 @@
 int main(void)
 {
     greeting(“Developer A,B,C !!”);
+
+    farewell(“Developer A,B,C”);
     return 0;
 }
 
@@ -11,5 +13,13 @@ int main(void)
 int greeting(const char *name)
 {
     printf("Hello, %s!\n", name);
+    return 0;
+}
+
+// Print a farewell message forthe specified user
+
+int farewell(const char *name)
+{
+    printf("Goodbye, %s!\n", name);
     return 0;
 }
